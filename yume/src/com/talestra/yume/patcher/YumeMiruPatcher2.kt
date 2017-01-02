9df3c0e7949ba651449478462925d8198ab3b832
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-01-02 20:40:22 +0100

    Update dependencies

--- yume/src/com/talestra/yume/patcher/YumeMiruPatcher2.kt
+++ yume/src/com/talestra/yume/patcher/YumeMiruPatcher2.kt
@@ -7,14 +7,19 @@ import com.soywiz.korui.Application
 import com.soywiz.korui.frame
 import com.soywiz.korui.ui.button
 import com.soywiz.korui.ui.image
+import com.soywiz.korui.ui.vertical
 
 object YumeMiruPatcher2 {
 	@JvmStatic fun main(args: Array<String>) = EventLoop.main {
 		val bmp = ResourcesVfs["data/bg.jpg"].readBitmap()
+		val frameIcon = ResourcesVfs["patcher_ico.png"].readBitmap()
 		Application().frame("Yume Miru Kusuri en español - $VERSION", 640, 480) {
+			icon = frameIcon
 			image(bmp)
-			button("Patch") {
-				alert("Patching!")
+			vertical {
+				button("Patch") {
+					alert("Patching!")
+				}
 			}
 		}
 	}
